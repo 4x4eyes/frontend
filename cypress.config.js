737e3224c1f97d4fc7a87e150f8280aa8ab30195
Rawan-Hamza <rawanhamza@hotmail.com>
2023-01-26 15:41:21 +0000

    add cypress testing with login session

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("cypress");
 
-require("dotenv").config();
+require('dotenv').config()
 
 module.exports = defineConfig({
   e2e: {
@@ -15,4 +15,14 @@ module.exports = defineConfig({
       bundler: "webpack",
     },
   },
+
+  env: {
+    auth0_username: "fourbyfoureyes@yahoo.com",
+    auth0_password: "Fourx4eyesdev$",
+    auth0_domain: "httpsdev-rba1pzy2o2f8we1a.us.auth0.com",
+    auth0_audience:"https://dev-rba1pzy2o2f8we1a.us.auth0.com/api/v2/",
+    auth0_scope: "openid%20profile%20email&",
+    auth0_client_id: "K75OSESBnYEHisdJXkM6YPIF15aQyw8K",
+  }
+
 });
--- cypress/e2e/frontend.cy.js
+++ cypress/e2e/frontend.cy.js
@@ -1,4 +1,23 @@
 describe("root page", () => {
+  beforeEach(() => {
+    cy.session("login", () => {
+      cy.visit("http://localhost:3000/");
+      cy.contains("h1", "Binder").should("be.visible");
+      cy.contains("button", "Log In").click();
+
+      cy.origin("https://dev-rba1pzy2o2f8we1a.us.auth0.com/", () => {
+        cy.get("input#username").type(Cypress.env("auth0_username"), {
+          log: false,
+        });
+        cy.get("input#password").type(Cypress.env("auth0_password"), {
+          log: false,
+        });
+        cy.contains("button", "Continue").click();
+        cy.location("pathname").should("equal", null);
+      });
+    });
+  });
+
   it("passes find-matches", () => {
     cy.visit("localhost:3000");
 
