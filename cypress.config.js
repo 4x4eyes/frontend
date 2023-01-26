7f01b5882c7736f7ddc2e945f5e17acff576217f
Rawan-Hamza <rawanhamza@hotmail.com>
2023-01-26 13:39:47 +0000

    cypress login function

--- cypress.config.js
+++ cypress.config.js
@@ -17,11 +17,12 @@ module.exports = defineConfig({
   },
 
   env: {
-    auth0_username: process.env.AUTH0_USERNAME,
+    auth0_username: "fourbyfoureyes@yahoo.com",
     auth0_password: process.env.AUTH0_PASSWORD,
-    auth0_domain: process.env.REACT_APP_AUTH0_DOMAIN,
+    auth0_domain: "httpsdev-rba1pzy2o2f8we1a.us.auth0.com",
     auth0_audience: process.env.REACT_APP_AUTH0_AUDIENCE,
     auth0_scope: process.env.REACT_APP_AUTH0_SCOPE,
     auth0_client_id: process.env.REACT_APP_AUTH0_CLIENTID,
   }
+
 });
